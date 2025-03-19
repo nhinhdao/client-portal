1661d3a3ed10b6ea0d99abfcd080f925dec10355
nhinhdao <ndao2803@gmail.com>
2025-03-19 16:53:24 -0400

    Refactored model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,5 @@ class Patient < Client
   # Patient inherits from Client model
 
   # Patient has many Dietitians (subset of Provider) through enrollments
-  # has_many :dietitians, -> { where(type: "Dietitian") }, through: :enrollments, source: :provider
   has_many :dietitians, -> { where(type: "Dietitian") }, through: :enrollments, source: :provider
 end
