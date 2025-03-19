ac521555bad854603147530e68f8f6b93c6eadc7
nhinhdao <ndao2803@gmail.com>
2025-03-19 16:53:05 -0400

    Refactored models

--- app/models/dietitian.rb
+++ app/models/dietitian.rb
@@ -1,11 +1,9 @@
 class Dietitian < Provider
   # Dietitian inherits from Provider model
-  # Dietitian can be create by calling Dietitian.create(provider_params)
-  # or: Provider.create(provider_params, type: 'Dietitian')
 
-  # Dietitian has many Patients through enrollments
+  # Dietitian has many Patients (subset of clients) through enrollments
   has_many :patients, -> { where(type: "Patient") }, through: :enrollments, source: :client
 
   # Dietitian has many journal entries through clients, alias patients_journals
-  has_many :patients_journals, through: :clients, source: :journal_entries
+  has_many :patients_journals, through: :patients, source: :journal_entries
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,7 @@
 class Patient < Client
   # Patient inherits from Client model
-  # Patient can be create by calling Patient.create(client_params)
-  # or: Client.create(client_params, type: 'Patient')
 
-  # Patient has many Dietitians through enrollments
+  # Patient has many Dietitians (subset of Provider) through enrollments
+  # has_many :dietitians, -> { where(type: "Dietitian") }, through: :enrollments, source: :provider
   has_many :dietitians, -> { where(type: "Dietitian") }, through: :enrollments, source: :provider
 end
